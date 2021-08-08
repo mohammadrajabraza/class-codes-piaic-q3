3f2ef7aa5bdb68b16d9cb783b706cd06a5ab61bb
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-08-08 20:52:03 +0500

    fea: add service for echoing text

--- hyper_rust/src/main.rs
+++ hyper_rust/src/main.rs
@@ -1,6 +1,8 @@
 use std::{convert::Infallible, net::SocketAddr};
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Method, StatusCode};
+use futures::TryStreamExt as _;
 
 #[tokio::main]
 async fn main() {
@@ -12,7 +14,9 @@ async fn main() {
     // creates one from our `hello_world` function.
     let make_svc = make_service_fn(|_conn| async {
         // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_world))
+// ==>  
+        /* Ok::<_, Infallible>(service_fn(hello_world)) */
+        Ok::<_, Infallible>(service_fn(echo_service))
     });
 
     // Configuring server
@@ -24,7 +28,48 @@ async fn main() {
     }
 }
 
+// ==> 
+/*
 async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new("Hello, World".into()))
 }
+*/
 
+async fn echo_service(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        // root route to instruct users the usage of endpoints
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from
+            ("Try POSTing data to /echo\nOR Try POSTing data to /echo/uppercase");
+        },
+        // Route to handle requests that take input from body and 
+        // response back text 
+        (&Method::POST, "/echo") => {
+            *response.body_mut() = req.into_body();
+        },
+        // Route to handle requests that take input from body and 
+        // send response converted body text into Upper case 
+        (&Method::POST, "/echo/uppercase") => {
+            let mapping = req
+            .into_body()
+            .map_ok(|chunk| {
+                chunk.iter()
+                    .map(|byte| byte.to_ascii_uppercase())
+                    .collect::<Vec<u8>>()
+            });
+    
+            // Use `Body::wrap_stream` to convert it to a `Body`...
+            *response.body_mut() = Body::wrap_stream(mapping);
+        },
+        // Fallback route to handle all other requests other than specified above
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+            *response.body_mut() = Body::from("Requested resource not found at this endpoint!");
+        },
+    };
+
+    Ok(response)
+}
