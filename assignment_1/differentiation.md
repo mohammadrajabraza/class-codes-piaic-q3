de9fc6231d69c873784b34443b37ffdc3ad1d795
musama <usamakhan1984@hotmail.com>
2021-08-20 11:08:44 +0500

    Assignment-1_MUSAMA_PIAIC144165
    Multi Threading vs Async/Await

--- assignment_1/differentiation.md
+++ assignment_1/differentiation.md
@@ -1,3 +1,14 @@
-###Multi-threading
+>###Multi-threading
+> - Multi threading refers to CPU bound operations that can be run in parallel using seprate threads. 
+> - It requires multiple threads.
+> - It requires context switching by OS
 
-###Async
+> In example code we take the example of file download using 2 browser tabs each tab running on the seprate thread. Now 2 threads downloading the file seprately. We also mention the single thread excution just for the concept.
+
+
+>###Async / Await
+> - Async model refers to I/O bound operations waiting for another task to complete and upon the completion of the second task transfer the control to the next task. 
+> - It can be run with in the single thread. 
+> - It utilizes underlying programing framework to do the Task switching.
+
+> In example we take example of Morning routine i.e TakeShower - EatBreakfast - DressingUp - ReadNews the EatBreakfast and ReadNews can be done at the same time so they are asynchronously processing. The other 2 activities run synchronously.
--- assignment_1/src/main.rs
+++ assignment_1/src/main.rs
@@ -1,13 +1,99 @@
-// multi-traeding exaples/scenarion
+
+use std::time::Duration;
+use std::time::Instant; 
+
+// multi-threading examples/scenario
+use std::thread;
+
+fn download_file_browser_tab1(){
+    thread::sleep(Duration::from_secs(11));
+    println!("File from Browser Tab1 downloaded.");
+}
+
+fn download_file_browser_tab2(){
+    thread::sleep(Duration::from_secs(11));
+    println!("File from Browser Tab2 downloaded.");
+}
+
+fn download_files_on_single_thread()
+{
+    thread::spawn(|| {
+        let start = Instant::now();
+        let elapsed = start.elapsed();
+        download_file_browser_tab1();
+        download_file_browser_tab2();
+        println!("Time elapsed {:?}", elapsed);
+    });
+}
+
+fn download_files_in_parallel(){
+
+    let  start = Instant::now();
+    let  elapsed = start.elapsed();
+
+    let thread1 = thread::spawn(|| {
+        download_file_browser_tab1();
+    });
+
+    let thread2 = thread::spawn(|| {
+        download_file_browser_tab2();
+    });
+
+    thread1.join().expect("Thread 1 panicked!");
+    thread2.join().expect("Thread 2 panicked!");
+
+    println!("Time elapsed {:?}", elapsed);
+}
 
 
+// Async examples/scenario
+use async_std::task;
+use futures::executor::block_on;
 
+fn take_shower() {
+    println!("Taking shower");
+    thread::sleep(Duration::from_secs(7));
+    println!("Finished taking shower");
+}
 
+async fn eat_breakfast() {
+    println!("Eating breakfast");
+    thread::sleep(Duration::from_secs(4));
+    println!("Finished eating breakfast");
+}
 
+fn get_dressed() {
+    println!("Puton dress");
+    thread::sleep(Duration::from_secs(6));
+    println!("Finished dressing");
+}
 
-// Async examples/scenarion
+async fn read_news() {
+    println!("Reading news");
+    thread::sleep(Duration::from_secs(4));
+    println!("Finished reading news");
+}
+
+async fn morning_routine(){
+    take_shower(); 
+    task::spawn(eat_breakfast()); //async
+    let _ = read_news().await;
+    get_dressed();
+      
+}
 
 
 fn main() {
     println!("Assignment 1");
+
+    println!("1- Multi-Threading Example");
+    println!("Multiple files downloading using Single thread...");
+    download_files_on_single_thread();
+    thread::sleep(Duration::from_secs(25));
+    println!("Multiple files downloading using Parallel threads...");
+    download_files_in_parallel();
+    
+    println!("2- Async Example");
+    block_on(morning_routine());
+    
 }
