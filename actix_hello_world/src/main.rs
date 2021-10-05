113483c2726b2776dfefe499a0c4dc949d765b21
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-10-05 23:33:30 +0500

    fea: add multiple endpoints to serve different web pages

--- actix_hello_world/src/main.rs
+++ actix_hello_world/src/main.rs
@@ -17,6 +17,10 @@ async fn main() -> std::io::Result<()> {
         // Step 6
         App::new()
             .service(web::resource("/").route(web::get().to(index)))
+            .service(web::resource("/about").route(web::get().to(about)))
+            .service(web::resource("/services").route(web::get().to(services)))
+            .service(web::resource("/products").route(web::get().to(products)))
+            .service(web::resource("/contact").route(web::get().to(contact)))
             .service(web::resource("/hello").route(web::get().to(hello)))
             
     })
@@ -41,6 +45,30 @@ async fn index() -> Result<HttpResponse> {
     Ok(HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(include_str!("../static/index.html")))
+}
+
+async fn about() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/about.html")))
+}
+
+async fn services() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/services.html")))
+}
+
+async fn products() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/products.html")))
+}
+
+async fn contact() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/contact.html")))
 }
 
 async fn hello() -> Result<HttpResponse> {
