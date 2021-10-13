c833286f2fb37cf1430da6f493255be7931dd8df
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-10-13 19:29:16 +0500

    fea: add log messages to handler functions

--- actix_hello_world/src/main.rs
+++ actix_hello_world/src/main.rs
@@ -1,6 +1,6 @@
 // step 2
 use actix_web::{
-    middleware, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result,
+    web, App, HttpResponse, HttpServer, Result,
 };
 // Step 8
 use dotenv::dotenv;
@@ -9,6 +9,7 @@ use std::env;
 // Step 3
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    println!("Starting actix-web server");
     // Step 9
     dotenv().ok();
     // Step 4
@@ -16,12 +17,12 @@ async fn main() -> std::io::Result<()> {
 
         // Step 6
         App::new()
-            .service(web::resource("/").route(web::get().to(index)))
             .service(web::resource("/about").route(web::get().to(about)))
             .service(web::resource("/services").route(web::get().to(services)))
             .service(web::resource("/products").route(web::get().to(products)))
             .service(web::resource("/contact").route(web::get().to(contact)))
             .service(web::resource("/hello").route(web::get().to(hello)))
+            .service(web::resource("/").route(web::get().to(index)))
             
     })
     // Step 10
@@ -48,24 +49,28 @@ async fn index() -> Result<HttpResponse> {
 }
 
 async fn about() -> Result<HttpResponse> {
+    println!("User visited about page");
     Ok(HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(include_str!("../static/about.html")))
 }
 
 async fn services() -> Result<HttpResponse> {
+    println!("User visited services page");
     Ok(HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(include_str!("../static/services.html")))
 }
 
 async fn products() -> Result<HttpResponse> {
+    println!("User visited products page");
     Ok(HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(include_str!("../static/products.html")))
 }
 
 async fn contact() -> Result<HttpResponse> {
+    println!("User visited contact page");
     Ok(HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(include_str!("../static/contact.html")))
